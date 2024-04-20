6565c99ac6a7bd1ae8b0c348a42c6a3a390a2278
Ehabw57 <maestor.ehab123@gmail.com>
2024-04-21 01:21:21 +0200

    lol falsk

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,12 @@
 # installs flask version 2.1.0
 
- package {'falsk':
+ package {'flask':
    ensure   => '2.1.0',
-   name     => 'flask',
    provider => 'pip3',
  }
 
  package {'Werkzeug':
    ensure   => '2.1.1',
-   name     => 'werkzeug',
    provider => 'pip3',
  }
 
