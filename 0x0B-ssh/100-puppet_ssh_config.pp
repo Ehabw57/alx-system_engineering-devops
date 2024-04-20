894f025fb988351e3edb40320f6888e0895a3ff2
Ehabw57 <maestor.ehab123@gmail.com>
2024-04-21 01:12:39 +0200

    sorry wrong path

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Configure client side of the server using Puppet
 file { 'ssh_config':
   ensure => 'present',
-  path   => '~/.ssh/ssh_config',
+  path   => 'etc/ssh/ssh_config',
 }
 
 # Turn off password authentication line
 file_line { 'Turn off password auth':
   ensure  => 'present',
-  path    => '~/.ssh/ssh_config',
+  path    => 'etc/ssh/ssh_config',
   line    => 'BatchMode yes',
   replace => true,
   require => File['ssh_config'],
@@ -16,7 +16,7 @@ file_line { 'Turn off password auth':
 # Declare identity private key file to login
 file_line { 'Declare identity':
   ensure  => 'present',
-  path    => '~/.ssh/ssh_config',
+  path    => 'etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school',
   replace => true,
   require => File['ssh_config'],
