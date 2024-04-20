29ff81211e7adf44fe63aa0bc74223921edabdd4
Ehabw57 <maestor.ehab123@gmail.com>
2024-04-20 12:41:52 +0200

    how how

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,14 @@
 # installs flask version 2.1.0
 
- package {'falsk2.1.0':
-   ensure => '2.1.0',
-   name => 'flask',
+ package {'falsk':
+   ensure   => '2.1.0',
+   name     => 'flask',
+   provider => 'pip3',
+ }
+
+ package {'Werkzeug':
+   ensure   => '2.1.1',
+   name     => 'werkzeug',
    provider => 'pip3',
  }
 
