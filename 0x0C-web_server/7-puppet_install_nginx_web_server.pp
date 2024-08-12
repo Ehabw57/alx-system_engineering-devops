4881313acc6b1eee61c8b346ba1120cdfea8ab42
Ehabw57 <maestor.ehab123@gmail.com>
2024-08-13 00:45:36 +0300

    we love puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,56 @@
+#!/usr/bin/puppet apply
+# configre nginx server with puppet
+
+$config="server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+        location /redirect_me {
+                return 301 https://www.behance.net/EHABABDELRADY57;
+        }
+
+        error_page 404 /404.html;
+        location = /404.html {
+        root /var/www/html;
+        internal;
+        }
+}\n"
+
+exec { "apt-update":
+	command => "apt-get update",
+	path => "/usr/bin",
+}
+
+package { "nginx":
+	ensure => "installed",
+	require => Exec['apt-update'],
+}
+
+file {"/var/www/html/404.html":
+	ensure => file,
+	content => "Ceci n'est pas une page\n",
+}
+
+file {"/var/www/html/index.html":
+	ensure => file,
+	content => "Hello world!\n",
+}
+
+file {"/etc/nginx/sites-available/default":
+	ensure => file,
+	content => $config,
+}
+
+service {"nginx":
+	ensure => 'running',
+	enable => 'true',
+	require => Package["nginx"],
+	subscribe => [File['/var/www/html/index.html'], File['/var/www/html/404.html'],
+  File['/etc/nginx/sites-available/default']],
+}
+
