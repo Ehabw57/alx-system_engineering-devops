213e09321182403620d38661cc0f33a85893b4c9
Ehabw57 <maestro.ehab123@gmail.com>
2025-03-01 09:29:34 +0200

    lets see

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Gather data from an API """
+if __name__ == "__main__":
+    import csv
+    import requests
+    from sys import argv
+
+    if not argv[1].isdigit():
+        exit()
+
+    userid = argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    response = requests.get(f"{url}users/{userid}").json()
+    name = response.get("name")
+    response = requests.get(f"{url}todos", params={"userId": userid}).json()
+
+    with open(f'{argv[1]}.csv', 'w') as Csvfile:
+        writer = csv.writer(Csvfile, delimiter=' ')
+        for task in response:
+            writer.writerow([argv[1], name, task["completed"], task["title"]])
