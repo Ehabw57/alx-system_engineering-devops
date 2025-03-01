27b3cb3b3c09cfb9c992a646a01fd0d2c77f11b1
Ehabw57 <maestro.ehab123@gmail.com>
2025-03-01 10:59:10 +0200

    whof filtering is so frustration

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Gather data from an API """
+if __name__ == "__main__":
+    import json
+    import requests
+
+    url = "https://jsonplaceholder.typicode.com"
+    all_tasks = requests.get(f"{url}/todos").json()
+    data = {}
+    for task in all_tasks:
+        userId = str(task['userId'])
+        name = requests.get(f"{url}/users/{userId}").json()["username"]
+        if not data.get(userId):
+            data[userId] = []
+        data[userId].append({
+            "task": task["title"],
+            "completed": task["completed"],
+            "username": name})
+
+    with open('todo_all_employees.json', 'w') as JsonFile:
+        dumped = json.dumps(data)
+        JsonFile.write(dumped)
